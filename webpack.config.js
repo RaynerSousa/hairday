f67f060b7e40e80e8a1eb68ba57e29a10a8257bd
Rayner Sousa <raynerwhanyfek@gmail.com>
2026-04-27 19:29:22 -0300

    add the HTML yo webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
+const { stat } = require("fs")
 const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
 
 module.exports = {
     target: "web",
@@ -7,6 +9,19 @@ module.exports = {
     entry: path.resolve(__dirname, "src", "main.js"),
     output: {
         filename: "main.js",
-        path: path.resolve(__dirname, "dist")
-    }
+        path: path.resolve(__dirname, "dist"),
+    },
+
+    devServer: {
+        static: {
+            directory: path.join(__dirname, "dist"),
+        },
+        port: 3080,
+        open: true,
+        liveReload: true,
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: path.resolve(__dirname,"index.html"),
+        favicon: path.resolve("src","assets","scissors.svg")
+    })]
 }
