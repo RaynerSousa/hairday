e4ee94d0217052a6f4953cc8d260d5b5b2165af5
Rayner Sousa <raynerwhanyfek@gmail.com>
2026-04-28 19:22:12 -0300

    finalizando

--- src/main.js
+++ src/main.js
@@ -1,14 +1,13 @@
 "use strict"
 
-// configuração do dayjs
-import dayjs from "dayjs"
+import "./libs/dayjs.js"
 
-//CSS
-import "./styles/form.css"
 import "./styles/global.css"
+import "./styles/form.css"
 import "./styles/schedule.css"
 
+import "./modules/form/submit.js"
+import "./modules/form/date-change.js"
+import "./modules/schedules/cancel.js"
 
-//Js
-import "./modules/form/submit.js";
 import "./modules/page-load.js"
